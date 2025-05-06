cf8a8b9a2a96fed9cdea75f023785cfd10b64437
Samuel <darkstronout@gmail.com>
2025-05-06 20:59:52 +0800

    update header

--- frontend/src/components/forms/header-search-form.jsx
+++ frontend/src/components/forms/header-search-form.jsx
@@ -20,7 +20,7 @@ const HeaderSearchForm = () => {
             onChange={(e) => setSearchText(e.target.value)}
             value={searchText}
             type="text"
-            placeholder="Cari Produk..."
+            placeholder="Cari Produk Anda..."
           />
         </div>
         <div className="tp-header-search-category">
