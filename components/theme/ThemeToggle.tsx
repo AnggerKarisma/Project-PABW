d95ddd58a1ee3561c3c8a7ec39ed1120bc962fd3
Ardi11221026 <11221026@student.itk.ac.id>
2025-05-14 09:21:12 +0800

    update dikit

--- components/theme/ThemeToggle.tsx
+++ components/theme/ThemeToggle.tsx
@@ -18,21 +18,21 @@ export function ThemeToggle() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="link" className="hover:bg-gray-200 dark:hover:bg-gray-800 duration-200  p-2 " size="icon">
+        <Button variant="link" className="hover:bg-gray-200 dark:hover:bg-gray-800 duration-200 p-2" size="icon">
           <Sun className="h-[1.5rem] w-[1.5rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute h-[1.5rem] w-[1.5rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
+          <span className="sr-only">Ubah tema</span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end" className="rounded-xl">
         <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
+          Terang
         </DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
+          Gelap
         </DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
+          Sistem
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
